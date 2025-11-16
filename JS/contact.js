50add443b742a53b564f98030876857a5fc84773
swissweden <josan.ryujs@gmail.com>
2025-11-16 17:43:29 +0900

    쓸데없는 주석 삭제 및 텍스트 변경

--- JS/contact.js
+++ JS/contact.js
@@ -6,13 +6,11 @@ document.addEventListener('DOMContentLoaded', () => {
     
     if (contactForm) {
         contactForm.addEventListener('submit', (e) => {
-            // 1. 폼의 기본 전송 기능(페이지 새로고침)을 막습니다.
+
             e.preventDefault(); 
             
-            // 2. 데모 알림창을 띄웁니다.
-            alert('메시지가 성공적으로 전송되었습니다! (데모)');
-            
-            // 3. (선택사항) 폼의 입력 내용을 초기화합니다.
+            alert('메시지가 성공적으로 전송되었습니다! (예시입니다.)');
+
             contactForm.reset();
         });
     }
